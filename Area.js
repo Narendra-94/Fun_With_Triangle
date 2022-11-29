2ff7f7211253cbf29da5a11ecd6012e87fa90471
Narendra <narenchordiya07@gmail.com>
2022-11-29 23:55:55 +0530

    feat: wrote condition for negative number check

--- Area.js
+++ Area.js
@@ -3,15 +3,19 @@ const inputs = document.querySelectorAll(".input");
 const button = document.querySelector("#btn");
 const output = document.querySelector("#output");
 
-function AreaOfTri(base, height){
-    const area = (base * height)/2;
-    return area;
+function AreaOfTri(base, height) {
+  const area = (base * height) / 2;
+  return area;
 }
 
-function calAreaOfTri(){
-    const area = AreaOfTri(Number(inputs[0].value),Number(inputs[1].value));
+function calAreaOfTri() {
+  const area = AreaOfTri(Number(inputs[0].value), Number(inputs[1].value));
+  if (area >= 0) {
     output.innerText = "The area of the triangle is " + area + " cm²";
-
+  } else {
+    output.innerText =
+      "Area of triangle can't be negative. Please enter valid Number";
+  }
 }
 
-button.addEventListener('click', calAreaOfTri);
+button.addEventListener("click", calAreaOfTri);
